8c9e387329c45be6f0821d2883fb88e328899c44
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2020-11-16 18:02:27 +0100

    Complete ex 4, mvcio2

--- src/it/unibo/oop/lab/mvcio2/SimpleGUIWithFileChooser.java
+++ src/it/unibo/oop/lab/mvcio2/SimpleGUIWithFileChooser.java
@@ -1,11 +1,23 @@
 package it.unibo.oop.lab.mvcio2;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import it.unibo.oop.lab.mvcio.Controller;
 
 /**
  * A very simple program using a graphical interface.
  * 
  */
 public final class SimpleGUIWithFileChooser {
-
     /*
      * TODO: Starting from the application in mvcio:
      * 
@@ -31,5 +43,80 @@ public final class SimpleGUIWithFileChooser {
      * update the UI: in this example the UI knows when should be updated, so
      * try to keep things separated.
      */
+    private final JFrame frame = new JFrame();
+    private final Controller c;
+    
+    public SimpleGUIWithFileChooser(final Controller c) {
+        this.c = c;
+        /*
+         * Make the frame half the resolution of the screen. This very method is
+         * enough for a single screen setup. In case of multiple monitors, the
+         * primary is selected.
+         * 
+         * In order to deal coherently with multimonitor setups, other
+         * facilities exist (see the Java documentation about this issue). It is
+         * MUCH better than manually specify the size of a window in pixel: it
+         * takes into account the current resolution.
+         */
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / 2, sh / 2);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        /*
+         * Instead of appearing at (0,0), upper left corner of the screen, this
+         * flag makes the OS window manager take care of the default positioning
+         * on screen. Results may vary, but it is generally the best choice.
+         */
+        frame.setLocationByPlatform(true);
+        final JPanel canvas = new JPanel();
+        canvas.setLayout(new BorderLayout());
+        final JTextArea textArea = new JTextArea();
+        canvas.add(textArea, BorderLayout.CENTER);
+        final JButton save = new JButton("Save");
+        canvas.add(save, BorderLayout.SOUTH);
+        final JPanel browsePanel = new JPanel();
+        browsePanel.setLayout(new BorderLayout());
+        final JTextField filePath = new JTextField();
+        filePath.setEditable(false);
+        filePath.setText(c.getCurrentFilePath());
+        browsePanel.add(filePath, BorderLayout.CENTER);
+        final JButton browse = new JButton("Browse...");
+        browsePanel.add(browse, BorderLayout.LINE_END);
+        canvas.add(browsePanel, BorderLayout.NORTH);
+        frame.setContentPane(canvas);
+        /*
+         * EventListeners
+         */
+        save.addActionListener(new ActionListener() {
+            public void actionPerformed(final ActionEvent e) {
+                c.writeOnFile(textArea.getText());
+            }
+        });
+        browse.addActionListener(new ActionListener() {
+            public void actionPerformed(final ActionEvent e) {
+                final JFileChooser fc = new JFileChooser(c.getCurrentFile());
+                final int fcChoice = fc.showOpenDialog(frame);
+                switch (fcChoice) {
+                case JFileChooser.APPROVE_OPTION:
+                    c.setCurrentFile(fc.getSelectedFile());
+                    filePath.setText(c.getCurrentFilePath());
+                    break;
+                case JFileChooser.CANCEL_OPTION:
+                    break;
+                case JFileChooser.ERROR_OPTION:
+                    JOptionPane.showMessageDialog(frame, "An error as occurred");
+                    break;
+                default:
+                    break;
+                }
+            }
+        });
+    }
+    
+    public static void main(final String[] args) {
+        final SimpleGUIWithFileChooser gui = new SimpleGUIWithFileChooser(new Controller());
+        gui.frame.setVisible(true);
+    }
 
 }
